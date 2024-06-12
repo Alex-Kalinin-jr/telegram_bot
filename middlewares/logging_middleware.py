43a056eac34b81ca24b976c57d11a97ce9897aa5
Curse Bowman <cursebow@si-d4.nsk.21-school.ru>
2024-06-12 16:09:38 +0700

    user detection added

--- middlewares/logging_middleware.py
+++ middlewares/logging_middleware.py
@@ -24,5 +24,6 @@ async def handle_outer_middleware(handler, event, data):
 async def logging_middleware(handler, event, data):
     flags_data = extract_flags(data)
     timedelta = time.clock_gettime(time.CLOCK_MONOTONIC) - data["time_start"]
-    logger.debug(f"{flags_data['name']} : {timedelta}")
+    logger.debug(f"user_id: {event.from_user.id}, username: {event.from_user.username}, \
+user full name: {event.from_user.first_name} {event.from_user.last_name} ___ {flags_data['name']} : {timedelta}")
     return await handler(event, data)
