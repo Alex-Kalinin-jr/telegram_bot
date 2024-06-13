b5aab36691e8f33160e348c74f5062dbea2dba25
Alex <Kalinin-Alexey-Russia@yandex.ru>
2024-06-13 05:40:48 +0000

    redis connection establishing failure

--- bot_service/bot/bot.py
+++ bot_service/bot/bot.py
@@ -5,11 +5,13 @@ from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.fsm.storage.memory import MemoryStorage
 
-from config import BOT_TOKEN
+from config import BOT_TOKEN, REDIS_URL
 from routers.test_router import router
 from database.db import BotDB
 from aiogram.fsm.storage.redis import RedisStorage, Redis
 from routers.admin_router import r_admin
+
+
 
 
 logger = logging.getLogger(__name__)
@@ -19,14 +21,14 @@ logging.basicConfig(
 
 )
 
+
 db_instance = BotDB('database.db')
-storage = MemoryStorage()
-# redis = Redis(host='localhost')
-# storage = RedisStorage(redis=redis)
+# storage = MemoryStorage()
+redis = Redis(host=REDIS_URL)
+storage = RedisStorage(redis=redis)
 
 dp = Dispatcher(storage=storage)
 dp['db_instance'] = db_instance
 dp.include_router(r_admin)
 dp.include_router(router)
-# dp.middleware(ThrottlingMiddleware(storage=storage, timeout=0.5))
 bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
