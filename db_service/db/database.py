cbd017086afea54b7e7fb1a5e34bf13ef8776be8
alex <Kalinin-Alexey-russia@yandex.ru>
2024-06-14 12:46:46 +0000

    unnecessary logging deleted

--- db_service/db/database.py
+++ db_service/db/database.py
@@ -11,14 +11,10 @@ from .models import *
 
 logger = logging.getLogger(__name__)
 formatter = logging.Formatter('%(levelname)s - %(message)s')
-handler = logging.FileHandler('functions_time.log')
+handler = logging.FileHandler('db_log.log')
 handler.setFormatter(formatter)
 logger.addHandler(handler)
-logger.debug("KDFJDKFJDKFJDKFJDSKFJDKFJDF")
-logger.debug("KDFJDKFJDKFJDKFJDSKFJDKFJDF")
-logger.debug("KDFJDKFJDKFJDKFJDSKFJDKFJDF")
-logger.debug("KDFJDKFJDKFJDKFJDSKFJDKFJDF")
-logger.error(F"this is url: {DB_URL}")
+
 engine = create_async_engine(DB_URL, echo=True, future=True)
 async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
